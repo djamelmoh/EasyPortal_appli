ffeb844707c2a05bdeeff414d9b36641455684da
djamelmoh <djamel.mohammedi@lycee-jeanrostand.fr>
2022-06-07 15:29:41 +0200

    Dossier

--- app/src/main/java/com/example/appli/Main3_supprimer_plaque_user.java
+++ app/src/main/java/com/example/appli/Main3_supprimer_plaque_user.java
@@ -2,41 +2,28 @@ package com.example.appli;
 
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Main3_supprimer_plaque_user extends AppCompatActivity {
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity3_page_user_supprimer);
-        Button button_retour = (Button) findViewById(R.id.retour1);
-        button_retour.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View btn_retour) {
-                Intent retour = new Intent(Main3_supprimer_plaque_user.this, Main2_page_users.class);
-                startActivity(retour);
-            }
-        });
-//---------------------------------------------------------------------------------------------------------
         //Page d'accueil pour acceder au autre fonctionnalité de l'admin
         EditText plaque_imma;
         plaque_imma = (EditText)findViewById(R.id.suppr_plaque);
